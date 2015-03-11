dc592e12006066394230b656b99a9dd036e10b3d
Nick Lockwood <nicklockwood@fb.com>
2015-03-11 13:53:30 -0700

    Unforked ExceptionsManager, AlertManager and AppState

--- Libraries/Network/RCTReachability.m
+++ Libraries/Network/RCTReachability.m
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
+// Copyright 2004-present Facebook. All Rights Reserved.
 
 #import "RCTReachability.h"
 
@@ -53,8 +46,6 @@ static void RCTReachabilityCallback(__unused SCNetworkReachabilityRef target, SC
   }
 }
 
-RCT_EXPORT_MODULE()
-
 #pragma mark - Lifecycle
 
 - (instancetype)initWithHost:(NSString *)host
@@ -83,9 +74,11 @@ RCT_EXPORT_MODULE()
 #pragma mark - Public API
 
 // TODO: remove error callback - not needed except by Subscribable interface
-RCT_EXPORT_METHOD(getCurrentReachability:(RCTResponseSenderBlock)getSuccess
-                  withErrorCallback:(__unused RCTResponseSenderBlock)getError)
+- (void)getCurrentReachability:(RCTResponseSenderBlock)getSuccess
+             withErrorCallback:(__unused RCTResponseSenderBlock)getError
 {
+  RCT_EXPORT();
+
   getSuccess(@[@{@"network_reachability": _status}]);
 }
 
