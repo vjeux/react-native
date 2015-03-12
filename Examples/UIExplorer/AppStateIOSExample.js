86e15dbd938329161af1fa9ae27be380991030c5
Eric Vicenti <evv@fb.com>
2015-03-11 20:48:43 -0700

    [ReactNative] AppState cleanup, remove Subscribable, add in OSS examples

--- Examples/UIExplorer/AppStateIOSExample.js
+++ Examples/UIExplorer/AppStateIOSExample.js
@@ -1,18 +1,7 @@
 /**
- * The examples provided by Facebook are for non-commercial testing and
- * evaluation purposes only.
- *
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright 2004-present Facebook. All Rights Reserved.
  *
  * @providesModule AppStateIOSExample
- * @flow
  */
 'use strict';
 
@@ -71,10 +60,10 @@ exports.examples = [
   {
     title: 'Subscribed AppStateIOS:',
     description: 'This changes according to the current state, so you can only ever see it rendered as "active"',
-    render(): ReactElement { return <AppStateSubscription showCurrentOnly={true} />; }
+    render() { return <AppStateSubscription showCurrentOnly={true} />; }
   },
   {
     title: 'Previous states:',
-    render(): ReactElement { return <AppStateSubscription showCurrentOnly={false} />; }
+    render() { return <AppStateSubscription showCurrentOnly={false} />; }
   },
 ];
