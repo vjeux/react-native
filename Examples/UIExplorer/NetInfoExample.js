af66b29f8be17c963107e5223e2671a1505e03bb
Eric Vicenti <evv@fb.com>
2015-03-16 13:57:43 -0700

    [ReactNative] NetworkInformation.reachability API w/ example

--- Examples/UIExplorer/NetInfoExample.js
+++ Examples/UIExplorer/NetInfoExample.js
@@ -1,17 +1,5 @@
 /**
- * The examples provided by Facebook are for non-commercial testing and
- * evaluation purposes only.
- *
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * @flow
+ * Copyright 2004-present Facebook. All Rights Reserved.
  */
 'use strict';
 
@@ -132,16 +120,16 @@ exports.examples = [
   {
     title: 'NetInfo.isConnected',
     description: 'Asyncronously load and observe connectivity',
-    render(): ReactElement { return <IsConnected />; }
+    render() { return <IsConnected />; }
   },
   {
     title: 'NetInfo.reachabilityIOS',
     description: 'Asyncronously load and observe iOS reachability',
-    render(): ReactElement { return <ReachabilityCurrent />; }
+    render() { return <ReachabilityCurrent />; }
   },
   {
     title: 'NetInfo.reachabilityIOS',
     description: 'Observed updates to iOS reachability',
-    render(): ReactElement { return <ReachabilitySubscription />; }
+    render() { return <ReachabilitySubscription />; }
   },
 ];
--- Libraries/Network/NetInfo.js
+++ Libraries/Network/NetInfo.js
@@ -1,10 +1,5 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * Copyright 2004-present Facebook. All Rights Reserved.
  *
  * @providesModule NetInfo
  * @flow
@@ -13,7 +8,7 @@
 
 var NativeModules = require('NativeModules');
 var RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
-var RCTReachability = NativeModules.Reachability;
+var RKReachability = NativeModules.RKReachability;
 
 var DEVICE_REACHABILITY_EVENT = 'reachabilityDidChange';
 
@@ -21,25 +16,18 @@ type ChangeEventName = $Enum<{
   change: string;
 }>;
 
-type ReachabilityStateIOS = $Enum<{
-  cell: string;
-  none: string;
-  unknown: string;
-  wifi: string;
-}>;
-
 
 /**
  * NetInfo exposes info about online/offline status
  *
- * ### reachabilityIOS
+ * == iOS Reachability
  *
  * Asyncronously determine if the device is online and on a cellular network.
  *
- * - `none` - device is offline
- * - `wifi` - device is online and connected via wifi, or is the iOS simulator
- * - `cell` - device is connected via Edge, 3G, WiMax, or LTE
- * - `unknown` - error case and the network status is unknown
+ * - "none" - device is offline
+ * - "wifi" - device is online and connected via wifi, or is the iOS simulator
+ * - "cell" - device is connected via Edge, 3G, WiMax, or LTE
+ * - "unknown" - error case and the network status is unknown
  *
  * ```
  * NetInfo.reachabilityIOS.fetch().done((reach) => {
@@ -57,37 +45,11 @@ type ReachabilityStateIOS = $Enum<{
  *   handleFirstReachabilityChange
  * );
  * ```
- *
- * ### isConnected
- *
- * Available on all platforms. Asyncronously fetch a boolean to determine
- * internet connectivity.
- *
- * ```
- * NetInfo.isConnected.fetch().done((isConnected) => {
- *   console.log('First, is ' + (isConnected ? 'online' : 'offline'));
- * });
- * function handleFirstConnectivityChange(isConnected) {
- *   console.log('Then, is ' + (isConnected ? 'online' : 'offline'));
- *   NetInfo.isConnected.removeEventListener(
- *     'change',
- *     handleFirstConnectivityChange
- *   );
- * }
- * NetInfo.isConnected.addEventListener(
- *   'change',
- *   handleFirstConnectivityChange
- * );
- * ```
  */
 
 var NetInfo = {};
 
-if (RCTReachability) {
-
-  // RCTReachability is exposed, so this is an iOS-like environment and we will
-  // expose reachabilityIOS
-
+if (RKReachability) {
   var _reachabilitySubscriptions = {};
 
   NetInfo.reachabilityIOS = {
@@ -116,8 +78,8 @@ if (RCTReachability) {
 
     fetch: function(): Promise {
       return new Promise((resolve, reject) => {
-        RCTReachability.getCurrentReachability(
-          function(resp) {
+        RKReachability.getCurrentReachability(
+          (resp) => {
             resolve(resp.network_reachability);
           },
           reject
@@ -126,42 +88,53 @@ if (RCTReachability) {
     },
   };
 
+  /**
+   *
+   * == NetInfo.isConnected
+   *
+   * Available on all platforms. Asyncronously fetch a boolean to determine
+   * internet connectivity.
+   *
+   * ```
+   * NetInfo.isConnected.fetch().done((isConnected) => {
+   *   console.log('First, is ' + (isConnected ? 'online' : 'offline'));
+   * });
+   * function handleFirstConnectivityChange(isConnected) {
+   *   console.log('Then, is ' + (isConnected ? 'online' : 'offline'));
+   *   NetInfo.isConnected.removeEventListener(
+   *     'change',
+   *     handleFirstConnectivityChange
+   *   );
+   * }
+   * NetInfo.isConnected.addEventListener(
+   *   'change',
+   *   handleFirstConnectivityChange
+   * );
+   * ```
+   *
+   */
   var _isConnectedSubscriptions = {};
-
-  var _iosReachabilityIsConnected = function(
-    reachability: ReachabilityStateIOS
-  ): bool {
-    return reachability !== 'none' &&
-      reachability !== 'unknown';
-  };
-
   NetInfo.isConnected = {
     addEventListener: function (
       eventName: ChangeEventName,
       handler: Function
     ): void {
       _isConnectedSubscriptions[handler] = (reachability) => {
-        handler(_iosReachabilityIsConnected(reachability));
+        handler(reachability !== 'none');
       };
-      NetInfo.reachabilityIOS.addEventListener(
-        eventName,
-        _isConnectedSubscriptions[handler]
-      );
+      NetInfo.reachabilityIOS.addEventListener(eventName, _isConnectedSubscriptions[handler]);
     },
 
     removeEventListener: function(
       eventName: ChangeEventName,
       handler: Function
     ): void {
-      NetInfo.reachabilityIOS.removeEventListener(
-        eventName,
-        _isConnectedSubscriptions[handler]
-      );
+      NetInfo.reachabilityIOS.removeEventListener(eventName, _isConnectedSubscriptions[handler]);
     },
 
     fetch: function(): Promise {
       return NetInfo.reachabilityIOS.fetch().then(
-        (reachability) => _iosReachabilityIsConnected(reachability)
+        (reachability) => reachability !== 'none'
       );
     },
   };
