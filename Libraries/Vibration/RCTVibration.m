d8fd4b6635a7b611c57da984ac6da41b47976bd7
Joe Stanton <joe.stanton@red-badger.com>
2015-03-16 21:02:57 -0700

    [RCTVibration] Basic Vibration API
    Summary:
    * Exposed as `VibrationIOS.vibrate();`
    * Included a UI Explorer example.
    * Vibration patterns are currently unsupported as there is no API to produce a vibration of less than 1 second on iOS.
    Closes https://github.com/facebook/react-native/pull/154
    Github Author: Joe Stanton <joe.stanton@red-badger.com>
    
    Test Plan: Imported from GitHub, without a `Test Plan:` line.

--- Libraries/Vibration/RCTVibration.m
+++ Libraries/Vibration/RCTVibration.m
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
+// Copyright 2004-present Facebook. All Rights Reserved.
 
 #import "RCTVibration.h"
 
@@ -13,10 +6,9 @@
 
 @implementation RCTVibration
 
-RCT_EXPORT_MODULE()
-
-RCT_EXPORT_METHOD(vibrate)
+- (void)vibrate
 {
+  RCT_EXPORT();
   AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
 }
 
