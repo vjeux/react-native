1b6672d1b242e4c20b1ebebe3b2b1b5e1a43c207
Spencer Ahrens <sahrens@fb.com>
2015-03-17 22:22:03 -0700

    [ReactNative] Strip prefixes from NativeModules keys

--- Libraries/Utilities/nativeModulePrefixNormalizer.js
+++ Libraries/Utilities/nativeModulePrefixNormalizer.js
@@ -1,20 +1,12 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * Copyright 2004-present Facebook. All Rights Reserved.
  *
  * @providesModule nativeModulePrefixNormalizer
- * @flow
  */
 'use strict';
 
 // Dirty hack to support old (RK) and new (RCT) native module name conventions
-function nativeModulePrefixNormalizer(
-  modules: {[key: string]: any}
-): void {
+function nativeModulePrefixNormalizer(modules) {
   Object.keys(modules).forEach((moduleName) => {
     var strippedName = moduleName.replace(/^(RCT|RK)/, '');
     if (modules['RCT' + strippedName] && modules['RK' + strippedName]) {
