57480799cfc9e72a696850d77b424ba68d0c484a
Spencer Ahrens <sahrens@fb.com>
2015-03-18 13:37:14 -0700

    [ReactNative] Add timers integration test

--- IntegrationTests/TimersTest.js
+++ IntegrationTests/TimersTest.js
@@ -1,10 +1,5 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * Copyright 2004-present Facebook. All Rights Reserved.
  */
 'use strict';
 
@@ -13,9 +8,9 @@ var React = require('react-native');
 var {
   StyleSheet,
   Text,
+  TimerMixin,
   View,
 } = React;
-var TimerMixin = require('react-timer-mixin');
 
 var TimersTest = React.createClass({
   mixins: [TimerMixin],
