98e5aa5492e871e8814d35a1ef7b4820189badf1
Tadeu Zagallo <tadeuzagallo@fb.com>
2015-03-25 18:59:42 -0700

    [ReactNative] Add deep linking api

--- Libraries/LinkingIOS/RCTLinkingManager.m
+++ Libraries/LinkingIOS/RCTLinkingManager.m
@@ -18,8 +18,6 @@ NSString *const RCTOpenURLNotification = @"RCTOpenURLNotification";
 
 @synthesize bridge = _bridge;
 
-RCT_EXPORT_MODULE()
-
 - (instancetype)init
 {
   if ((self = [super init])) {
@@ -37,11 +35,11 @@ RCT_EXPORT_MODULE()
 }
 
 + (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)URL
+            openURL:(NSURL *)url
   sourceApplication:(NSString *)sourceApplication
          annotation:(id)annotation
 {
-  NSDictionary *payload = @{@"url": [URL absoluteString]};
+  NSDictionary *payload = @{@"url": [url absoluteString]};
   [[NSNotificationCenter defaultCenter] postNotificationName:RCTOpenURLNotification
                                                       object:self
                                                     userInfo:payload];
@@ -54,22 +52,27 @@ RCT_EXPORT_MODULE()
                                               body:[notification userInfo]];
 }
 
-RCT_EXPORT_METHOD(openURL:(NSURL *)URL)
+- (void)openURL:(NSString *)url
 {
-  [[UIApplication sharedApplication] openURL:URL];
+  RCT_EXPORT();
+
+  [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
 }
 
-RCT_EXPORT_METHOD(canOpenURL:(NSURL *)URL
-                  callback:(RCTResponseSenderBlock)callback)
+- (void)canOpenURL:(NSString *)url
+          callback:(RCTResponseSenderBlock)callback
 {
-  BOOL canOpen = [[UIApplication sharedApplication] canOpenURL:URL];
-  callback(@[@(canOpen)]);
+  RCT_EXPORT();
+
+  BOOL supported = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:url]];
+  callback(@[@(supported)]);
 }
 
 - (NSDictionary *)constantsToExport
 {
-  NSURL *initialURL = _bridge.launchOptions[UIApplicationLaunchOptionsURLKey];
-  return @{@"initialURL": [initialURL absoluteString] ?: [NSNull null]};
+  return @{
+    @"initialURL": [[_bridge.launchOptions objectForKey:UIApplicationLaunchOptionsURLKey] absoluteString] ?: [NSNull null]
+  };
 }
 
 @end
